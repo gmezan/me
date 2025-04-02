f2d5c38cf0962d86190946e25f289a2571170cbe
Gustavo Meza <43119289+gmezan@users.noreply.github.com>
2025-04-02 02:29:37 -0500

    Projects and certifications (#5)
    * implementing base them
    
    * fix import
    
    * getting started
    
    * expertise and career history
    
    * projects and certs

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,10 +6,10 @@ function Footer() {
   return (
     <footer>
       <div>
-        <a href="https://github.com/yujisatojr" target="_blank" rel="noreferrer"><GitHubIcon/></a>
-        <a href="https://www.linkedin.com/in/yujisato/" target="_blank" rel="noreferrer"><LinkedInIcon/></a>
+        <a href="https://github.com/gmezan" target="_blank" rel="noreferrer"><GitHubIcon/></a>
+        <a href="https://www.linkedin.com/in/gmezan/" target="_blank" rel="noreferrer"><LinkedInIcon/></a>
       </div>
-      <p>A portfolio designed & built by <a href="https://github.com/yujisatojr/react-portfolio-template" target="_blank" rel="noreferrer">Yuji Sato</a> with 💜</p>
+      <p>My page - <a href="https://github.com/gmezan/me" target="_blank" rel="noreferrer">Gustavo Meza</a></p>
     </footer>
   );
 }
