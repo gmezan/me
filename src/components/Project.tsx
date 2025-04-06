d80b7044a253d63a5b53ca8b6209b3079f88b254
gmezan <meza.gustavo@pucp.edu.pe>
2025-04-06 15:30:52 -0500

    docs: spring + devops project

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -4,20 +4,26 @@ import levelfy from '../assets/images/levelfy.png';
 import snake from '../assets/images/snake.png';
 import rag from '../assets/images/rag.png'
 import infra from '../assets/images/infra.png'
+import sd from '../assets/images/spring_devops.png'
 
 import '../assets/styles/Project.scss';
 
 const projects = [
     {
-        id: 1,
         href: "https://github.com/gmezan/azure-openai-search-rag",
         title: "Chat with your Data app",
-        content: "AI powered RAG application using Azure OpenAI and AI Search. The agent answers questions related to Azure certifications exams. Developed and deployed the infra, backend and frontend.",
+        content: "AI powered RAG application using Azure OpenAI and AI Search. The agent answers questions related to Azure certifications exams. Developed and deployed the infra with terraform, backend with Spring & Java  and frontend with React.",
         imgSrc: rag,
         alt: "Chat with Data application interface"
     },
     {
-        id: 2,
+        href: "https://github.com/gmezan/azure-k8s-devops-java",
+        title: "CI/CD Pipeline for Spring Apps on Azure Kubernetes Service",
+        content: "Automated GitHub Actions pipelines deploy Terraform-defined Azure infrastructure (AKS, ACR) and Spring Boot apps to ACR. Azure AD federated credentials enable secure, secretless authentication for automated AKS deployments via IaC and unified CI/CD, eliminating manual steps from code to production.",
+        imgSrc: sd,
+        alt: "Chat with Data application interface"
+    },
+    {
         href: "https://github.com/gmezan/terraform-infra-gh-actions",
         title: "Infra CI/CD Management",
         content: "Developed with GH actions and terraform. CI/CD pipelines deploying to Azure and AWS cloud. Secured with encryption and GH secrets, support for multiple infra solutions for different projects.",
@@ -25,7 +31,6 @@ const projects = [
         alt: "CI/CD pipeline diagram"
     },
     {
-        id: 3,
         href: "https://github.com/gmezan/levelfy-frontend",
         title: "Levelfy",
         content: "Led the development of front and back-end for a web platform offering workshops created by and tailored to students. Developed with Angular and Java. Deployed infra, configured services, and managed operations using AWS.",
@@ -33,7 +38,6 @@ const projects = [
         alt: "Levelfy platform screenshot"
     },
     {
-        id: 4,
         href: "https://github.com/gmezan/ant-colony-implementation-for-tsp",
         title: "Ant Colony Optimization for TSP",
         content: "Multiple Ant Colony algorithm implementations to efficiently solve the traveling salesman problem (TSP).",
@@ -41,18 +45,16 @@ const projects = [
         alt: "Algorithm visualization"
     },
     {
-        id: 5,
         href: "https://github.com/gmezan/pucp-1iee15-digital-pulse-detector",
         title: "Digital Pulse Detector",
-        content: "Measured heart rate with sensors and ATmega328p microcontroller. Developed with C++ from scratch.",
+        content: "Measured heart rate with sensors and ATmega328p microcontroller. Developed in C++ from scratch.",
         imgSrc: dpd,
         alt: "Pulse detector hardware"
     },
     {
-        id: 6,
         href: "https://github.com/gmezan/snake-game-atmega32",
         title: "ATmega Snake Game",
-        content: "Developed snake game from scratch using 8-bits microcontroller ATmega32, 8x8 LED Matrix and transistors. Designed, manufactured, and programmed in C.",
+        content: "Developed snake game from scratch using 8-bits microcontroller ATmega32, 8x8 LED Matrix and transistors. Designed, manufactured, and developed in C language.",
         imgSrc: snake,
         alt: "Snake game on LED matrix"
     }
@@ -64,7 +66,7 @@ function Project() {
             <h1>Personal Projects</h1>
             <div className="projects-grid">
                 {projects.map((project) => (
-                    <div className="project" key={project.id}>
+                    <div className="project" key={project.href}>
                         <a href={project.href} target="_blank" rel="noreferrer">
                             <img 
                                 src={project.imgSrc} 
